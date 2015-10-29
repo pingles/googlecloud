f434afa6ddd98f528ebe4f354787abd9d8eafcb4
Paul Ingles <paul@oobaloo.co.uk>
2015-10-29 15:51:03 +0000

    add core subproject

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-sub "0.3.0"]]
-  :sub ["bigquery" "cloudstorage"]
+  :sub ["core" "bigquery" "cloudstorage"]
   :eval-in-leiningen true)
