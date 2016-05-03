8d31afb1c627d40f7293f85c479cbfa98317b056
Siddharth Dawara <siddharthdawara@gmail.com>
2016-05-03 17:18:30 +0100

    version bump for deploying

--- bigquery/project.clj
+++ bigquery/project.clj
@@ -1,4 +1,4 @@
-(defproject googlecloud/bigquery "0.3.8"
+(defproject googlecloud/bigquery "0.3.9"
   :description "BijQuery"
   :url "https://github.com/pingles/googlecloud"
   :license {:name "Eclipse Public License"
