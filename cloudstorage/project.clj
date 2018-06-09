48dd15aab11ed6e98e26ad9ee632ec560bfe773f
Daniel Bartz <daniel.bartz@freiheit.com>
2018-06-09 07:36:42 +0200

    raise version of gcs api

--- cloudstorage/project.clj
+++ cloudstorage/project.clj
@@ -1,10 +1,10 @@
-(defproject googlecloud/cloudstorage "0.3.4"
+(defproject googlecloud/cloudstorage "0.3.5"
   :description "Google Cloud Storage client"
   :url "https://github.com/pingles/googlecloud"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:dir ".."}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.google.api-client/google-api-client "1.20.0"]
-                 [com.google.apis/google-api-services-storage "v1-rev50-1.20.0"]
-                 [googlecloud/core "0.3.4"]])
+                 [com.google.api-client/google-api-client "1.23.0"]
+                 [com.google.apis/google-api-services-storage "v1-rev133-1.23.0"]
+                 [googlecloud/core "0.3.5"]])
